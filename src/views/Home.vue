47940f466b297032b46a582a2b5b6a928849404e
adowning <ashdowning@gmail.com>
2018-12-08 01:43:25 -0600

    Scripted auto-commit on change (2018-12-08 01:43:25) by gitwatch.sh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     <ion-button @click="showDialogAlert" full>Show Alert Box</ion-button>
     <ion-button @click="notify" full>Show Alert Box</ion-button>
     <ion-button @click="notify" full>Show Alert Box</ion-button>
+    <ion-button @click="notify" full>Show Alert Box</ion-button>
 
   </div>
 </template>
