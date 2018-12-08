1225fea403eca4ef35e5a193063fc6b70de0edb6
adowning <ashdowning@gmail.com>
2018-12-07 23:48:17 -0600

    Scripted auto-commit on change (2018-12-07 23:48:17) by gitwatch.sh

--- vue.config.js
+++ vue.config.js
@@ -6,26 +6,26 @@ module.exports = {
     },
     devServer: {
          host: 'localhost',
-        proxy:{ 
-            "/":  {
-            target: "https://ashdevtools.com:8080",
-            secure: true,
-            changeOrigin: true 
-        }
-    },
-        // disableHostCheck: true,
-        //  port: 8080,
-        //  hot: false,
-        //  https: true,
-        //       https: {
-        //     key: fs.readFileSync('./localhost.key'),
-        //     cert: fs.readFileSync('./localhost.crt'),
-        // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
-        // }
-         https: {
-            key: fs.readFileSync('/etc/ssl/private/cf.key'),
-            cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
-        //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
-        }
+    //     proxy:{ 
+    //         "/":  {
+    //         target: "https://ashdevtools.com:8080",
+    //         secure: true,
+    //         changeOrigin: true 
+    //     }
+    // },
+    //     // disableHostCheck: true,
+    //     //  port: 8080,
+    //     //  hot: false,
+    //     //  https: true,
+    //     //       https: {
+    //     //     key: fs.readFileSync('./localhost.key'),
+    //     //     cert: fs.readFileSync('./localhost.crt'),
+    //     // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
+    //     // }
+    //      https: {
+    //         key: fs.readFileSync('/etc/ssl/private/cf.key'),
+    //         cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
+    //     //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
+    //     }
     },
 }
