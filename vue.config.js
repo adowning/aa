14294833a0296963276a547cefa7a03840a4248e
adowning <ashdowning@gmail.com>
2018-12-07 23:37:18 -0600

    Scripted auto-commit on change (2018-12-07 23:37:18) by gitwatch.sh

--- vue.config.js
+++ vue.config.js
@@ -5,18 +5,21 @@ module.exports = {
         devtool: 'source-map',
     },
     devServer: {
-        host: '0.0.0.0',
-        changeOrigin: true,
-        proxy: 'https://localhost.com:8080',
+        // host: '0.0.0.0',
+        proxy: {
+            target: "https://localhost.com:8080",
+            secure: false,
+            changeOrigin: true 
+        },
         // disableHostCheck: true,
-         port: 8080,
-         hot: false,
-         https: true,
-              https: {
-            key: fs.readFileSync('./localhost.key'),
-            cert: fs.readFileSync('./localhost.crt'),
-        //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
-        }
+        //  port: 8080,
+        //  hot: false,
+        //  https: true,
+        //       https: {
+        //     key: fs.readFileSync('./localhost.key'),
+        //     cert: fs.readFileSync('./localhost.crt'),
+        // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
+        // }
         //  https: {
         //     key: fs.readFileSync('/etc/ssl/private/cf.key'),
         //     cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
