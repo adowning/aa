b8262195efe24ef8b10486d3086c19828398d82d
adowning <ashdowning@gmail.com>
2018-12-07 22:42:51 -0600

    Scripted auto-commit on change (2018-12-07 22:42:51) by gitwatch.sh

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,12 @@ module.exports = {
     devServer: {
         host: 'ashdevtools.com',
         // disableHostCheck: true,
-        port: 8080,
+        // port: 8080,
         // hot: true,
         // https: true,
          https: {
-          key: fs.readFileSync('/home/ash/aa/localkey.key'),
-          cert: fs.readFileSync('/home/ash/aa/local.crt'),
+            key: fs.readFileSync('/etc/ssl/private/cf.key'),
+            cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
         //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
         }
     },
