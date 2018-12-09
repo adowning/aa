0df1a44054a9a079238d1187db1a6c2ca66ed5c2
adowning <ashdowning@gmail.com>
2018-12-08 22:11:14 -0600

    Scripted auto-commit on change (2018-12-08 22:11:14) by gitwatch.sh

--- dashboard-1/accounts/app/services/serverSettingsService.js
+++ dashboard-1/accounts/app/services/serverSettingsService.js
@@ -2,7 +2,6 @@
 app.service('serverSettingsService', function($q,$http){ 
 
   this.isNewServer=function(){
-    return false;
     var q=$q.defer();
 
     $http.get(frontendServerURL+'/server/isNewServer').
@@ -17,20 +16,53 @@ app.service('serverSettingsService', function($q,$http){
 
 
   this.getServerSettings=function(){
+    var q=$q.defer();
 
-    let data = ["andrewsDev"]
-    return data;
-
-  //   var q=$q.defer();
-
-  //   $http.get(frontendServerURL+'/server').
-  //     success(function(data, status, headers, config) {
-  //       q.resolve(data);
-  //     }).
-  //     error(function(data, status, headers, config) {
-  //       q.reject(data);
-  //     });
-  //   return q.promise;
-  // }
+    $http.get(frontendServerURL+'/server').
+      success(function(data, status, headers, config) {
+        q.resolve(data);
+      }).
+      error(function(data, status, headers, config) {
+        q.reject(data);
+      });
+    return q.promise;
   }
+
 });
+
+
+// app.service('serverSettingsService', function($q,$http){ 
+
+//   this.isNewServer=function(){
+//     return false;
+//     var q=$q.defer();
+
+//     $http.get(frontendServerURL+'/server/isNewServer').
+//       success(function(data, status, headers, config) {
+//         q.resolve(data);
+//       }).
+//       error(function(data, status, headers, config) {
+//         q.reject(data);
+//       });
+//     return q.promise;
+//   }
+
+
+//   this.getServerSettings=function(){
+
+//     let data = ["andrewsDev"]
+//     return data;
+
+//   //   var q=$q.defer();
+
+//   //   $http.get(frontendServerURL+'/server').
+//   //     success(function(data, status, headers, config) {
+//   //       q.resolve(data);
+//   //     }).
+//   //     error(function(data, status, headers, config) {
+//   //       q.reject(data);
+//   //     });
+//   //   return q.promise;
+//   // }
+//   }
+// });
