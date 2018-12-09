84368e8a4d563644c5b18cf8a98c5f43007de61d
adowning <ashdowning@gmail.com>
2018-12-08 21:15:49 -0600

    asdf

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,37 @@
 const fs = require('fs')
 module.exports = {
-   
-    configureWebpack: {
-        devtool: 'source-map',
+   configureWebpack: {
+       devtool: 'source-map',
+   },
+
+   devServer: {
+        host: '0.0.0.0',
+       // proxy:{ 
+       //     "/":  {
+       //     target: "https://desktop:8080",
+       //     secure: true,
+       //     changeOrigin: true 
+       // },
+   // },
+      disableHostCheck: true,
+   //  port: 8081,
+   //  hot: true,
+    //    https: true,
+       //  https: {
+       //     key: fs.readFileSync('/home/ash/my-certificates/burp.key'),
+       //     cert: fs.readFileSync('/home/ash/my-certificates/burp-copy.pem'),
+       // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
+       // }
+       //  https: {
+       //     key: fs.readFileSync('/etc/ssl/private/cf.key'),
+       //     cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
+       // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
+       // }
     },
-    devServer: {
-         host: '0.0.0.0',
-        // proxy:{ 
-        //     "/":  {
-        //     target: "https://desktop:8080",
-        //     secure: true,
-        //     changeOrigin: true 
-        // },
-    // },
-       disableHostCheck: true,
-    //  port: 8081,
-    //  hot: true,
-     //    https: true,
-        //  https: {
-        //     key: fs.readFileSync('/home/ash/my-certificates/burp.key'),
-        //     cert: fs.readFileSync('/home/ash/my-certificates/burp-copy.pem'),
-        // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
-        // }
-        //  https: {
-        //     key: fs.readFileSync('/etc/ssl/private/cf.key'),
-        //     cert: fs.readFileSync('/etc/ssl/certs/cf.pem'),
-        // //   ca: fs.readFileSync('/home/ash/aa/localcerts/server_rootCA.pem'),
-        // }
-     },
+
+   baseUrl: '',
+
+   pluginOptions: {
+     cordovaPath: 'src-cordova'
+   }
 }
